d99f94e0112bbbf6c25ab5d5d0ff445b2e01db98
Jamie Stewart <stewart.jame.d@gmail.com>
2024-12-02 10:50:21 +0000

    Day 2

--- Days/day_02.cs
+++ Days/day_02.cs
@@ -11,17 +11,116 @@ namespace AoC_2024
 {
     class Day_02 : IDay
     {
+        class Record
+        {
+            public List<long> values = new();
+        }
+        private List<Record> ReadFileData()
+        {
+            List<Record> input = new();
+            using (StreamReader read = new StreamReader("./input/day_02.txt"))
+            {
+                string line;
+                while ((line = read.ReadLine()) != null)
+                {
+                    List<long> values = new();
+                    string[] lineContent = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    foreach( var value in lineContent )
+                    {
+                        if (long.TryParse(value, out long numValue))
+                        {
+                            values.Add(numValue);
+                        }                        
+                    }
+                    input.Add(new Record { values  = values } );
+                }
+            }
+            return input;
+        }
+
+        private bool testForIncrementalPass( List<long> record)
+        {
+            return record.Skip(1).Select((v, i) => v > (record[i]) && Math.Abs(v - (record[i])) <= 3).All(v => v);
+        }
+
+        private bool testForDecrementalPass(List<long> record)
+        {
+            return record.Skip(1).Select((v, i) => v < (record[i]) && Math.Abs(v - (record[i])) <= 3).All(v => v);
+        }
+        private long SolvePart1(List<Record> input)
+        {
+            //find safe rows
+            // Row is safe if all values are incrementing or decrementing with difference between adjacent values no greater than 3
+            long count = 0;
+           foreach(  var record in input )
+            {
+                bool decremental = testForDecrementalPass(record.values);
+                bool incremental = testForIncrementalPass(record.values);
+                if (incremental || decremental) { count++; }
+            }
+           return count;
+        }
+
+        private delegate bool TestPass( List<long> input );
+
+        private bool DampenerTestPass( List<long> record, TestPass testFunc )
+        {
+            if (testFunc(record))
+            {
+                return true;
+            }
+            else
+            {
+                //This could be sped up if the false value from the test was removed instead of testing by removing a single value across the record
+                for (int i = 0; i < record.Count; i++)
+                {
+                    List<long> values = new(record);
+                    values.RemoveAt(i);
+                    if (testFunc(values))
+                    {
+                        return true; ;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private long SolvePart2(List<Record> input)
+        {
+            //find safe rows - dampener allows 1 value to be removed from failing test to allow passing
+            // Row is safe if all values are incrementing or decrementing with difference between adjacent values no greater than 3
+            long count = 0;
+            TestPass increment = testForIncrementalPass;
+            TestPass decrement = testForDecrementalPass;
+
+            foreach (var record in input)
+            {
+                if(DampenerTestPass( record.values, increment))
+                {
+                    ++count;
+                }
+                else if( DampenerTestPass( record.values, decrement))
+                {
+                    ++count;
+                }
+                
+            }
+            return count;
+        }
         public Result Solve()
         {
             Stopwatch sw = Stopwatch.StartNew();
             sw.Start();
-
+            //Read input
+            List<Record> input = ReadFileData();
             //carry out tasks
+            long part_01 = SolvePart1(input);
+            long part_02 = SolvePart2(input);
 
             sw.Stop();
 
-            long time = sw.ElapsedMilliseconds / 1000L;
-            return new Result(name: "Day 02", part_01: 0L, part_02: 0L, execution_time: time);
+            double time = sw.ElapsedMilliseconds / 1000.0;
+            return new Result(name: "Day 02", part_01: part_01, part_02: part_02, execution_time: time);
         }
     }
 }
